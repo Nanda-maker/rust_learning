5d5d66e8650b0b7e3545097d4ca46b2a2770a4c8
Nanda-maker <nandakrgurung123@gmail.com>
2024-10-03 15:57:34 +0600

    structs

---
+++ sliceType/target/debug/deps/sliceType-203e4765cd9aff8b.d
@@ -0,0 +1,5 @@
+/Users/nanda/Desktop/rust-learning/sliceType/target/debug/deps/libsliceType-203e4765cd9aff8b.rmeta: src/main.rs
+
+/Users/nanda/Desktop/rust-learning/sliceType/target/debug/deps/sliceType-203e4765cd9aff8b.d: src/main.rs
+
+src/main.rs:
---
+++ sliceType/target/debug/deps/sliceType-6b8ec018edd2488c.d
@@ -0,0 +1,5 @@
+/Users/nanda/Desktop/rust-learning/sliceType/target/debug/deps/libsliceType-6b8ec018edd2488c.rmeta: src/main.rs
+
+/Users/nanda/Desktop/rust-learning/sliceType/target/debug/deps/sliceType-6b8ec018edd2488c.d: src/main.rs
+
+src/main.rs:
