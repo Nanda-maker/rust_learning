9ea18ac2df7e6564e179135e08c3a407b6d2d5ce
Nanda-maker <nandakrgurung123@gmail.com>
2024-10-03 15:56:53 +0600

    structs

---
+++ structs/target/debug/deps/structs-4c7affc7b8b094a2.d
@@ -0,0 +1,5 @@
+/Users/nanda/Desktop/rust-learning/structs/target/debug/deps/libstructs-4c7affc7b8b094a2.rmeta: src/main.rs
+
+/Users/nanda/Desktop/rust-learning/structs/target/debug/deps/structs-4c7affc7b8b094a2.d: src/main.rs
+
+src/main.rs:
